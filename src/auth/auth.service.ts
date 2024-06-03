57f30dc6bee421bdfcb83cf32f6857beca684475
Osmani Abderrahmane <abderrahmaneosmani2@gmail.com>
2024-06-03 19:39:31 +0100

    handle auth guard

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,22 +1,26 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
-// import { JwtService } from '@nestjs/jwt';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt'
 
 @Injectable()
 export class AuthService {
   constructor(
     private readonly user: UsersService,
-    // private readonly jwtService: JwtService,
+    private readonly jwtService: JwtService,
   ) {}
   async signIn(usernmae: string, pass: string): Promise<any> {
     const user = await this.user.findUserByEmail(usernmae);
-    if (user?.password !== pass) {
+
+    const isValidPass =await bcrypt.compare(pass,user.password)
+    
+    if (!isValidPass) {
       throw new UnauthorizedException();
     }
     const firstNameLastName = user.firstname + user.lastname;
     const payload = { sub: user.id, user: firstNameLastName };
     return {
-      // accessToken: await this.jwtService.signAsync(payload),
+      accessToken: await this.jwtService.signAsync(payload),
     };
   }
 }
